7c2a96d5fe6d4a2da4f1b6663fb970e3802be558
Said Molina <68343481+said-molina@users.noreply.github.com>
2023-08-14 20:59:34 -0600

    Update data.js

--- src/data/data.js
+++ src/data/data.js
@@ -1,12 +1,21 @@
  const projects = [
-    {
-      title: "Galleta de la Fortuna",
-      subtitle: "React",
+  {
+      title: "Rick and morty",
+      subtitle: "React Hooks",
       description:
-        "Proyecto basico con react, el cual muestra una galleta de la suerte aleatoria con estilos responsivos y componentes simples ",
-      image: "/fortune.png",
-      link: "https://fortune-cookiemx.netlify.app/",
+        "aplicacon que usa la API de Rick and Morty para mostrar las ubicaciones y los personajes que habitan en ella, al igual que muestra el estado del personaje",
+      image: "/rick-morty.png",
+      link: "https://rick-and-morty-app-sm.netlify.app/",
     },
+   {
+      title: "Wheater App",
+      subtitle: "React Hooks",
+      description:
+        "Aplicacion del tiempo el cual muestra el clima de la ciudad en la que te encuentras, cuenta con un boton que cambia los grados centigrados a farenheit",
+      image: "/weather.png",
+      link: "https://weather-app-sma.netlify.app/",
+    },
+    
     {
       title: "Shop",
       subtitle: "Javascript, HTML y CSS",
@@ -23,22 +32,16 @@
       image: "/users.png",
       link: "https://users-app-sm.netlify.app/",
     },
-    {
-      title: "Wheater App",
-      subtitle: "React Hooks",
+  {
+      title: "Galleta de la Fortuna",
+      subtitle: "React",
       description:
-        "Aplicacion del tiempo el cual muestra el clima de la ciudad en la que te encuentras, cuenta con un boton que cambia los grados centigrados a farenheit",
-      image: "/weather.png",
-      link: "https://weather-app-sma.netlify.app/",
+        "Proyecto basico con react, el cual muestra una galleta de la suerte aleatoria con estilos responsivos y componentes simples ",
+      image: "/fortune.png",
+      link: "https://fortune-cookiemx.netlify.app/",
     },
-    {
-      title: "Rick and morty",
-      subtitle: "React Hooks",
-      description:
-        "aplicacon que usa la API de Rick and Morty para mostrar las ubicaciones y los personajes que habitan en ella, al igual que muestra el estado del personaje",
-      image: "/rick-morty.png",
-      link: "https://rick-and-morty-app-sm.netlify.app/",
-    },
+   
+    
   ];
 
  
